dc2f3bab9bf13bea6e7270751bebc14ffaec9c60
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-11-03 20:48:33 -0500

    updated files

---
+++ 0x00-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/puppet
+# set up your client SSH configuration file so that you can connect to a server without typing a password
+
+file_line { 'indentity_file':
+  ensure => present,
+  line   => 'IdentityFile ~/.ssh/holberton',
+  path   => '/etc/ssh/ssh_config',
+}
+
+file_line { 'pwd_aut':
+  ensure => present,
+  line   => 'PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+}
