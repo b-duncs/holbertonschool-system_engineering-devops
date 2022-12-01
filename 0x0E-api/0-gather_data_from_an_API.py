5a0a6e23e22f89f5b0aadbea588a8cac42898758
Baylee Duncan <90226747+b-duncs@users.noreply.github.com>
2022-11-30 18:41:30 -0600

    updated files

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-"""
-using this REST API, for a given employee ID,
-returns information about his/her to-do list progress
-"""
-
+""" Grabs info from API """
 import json
 import sys
 from urllib import request
@@ -11,29 +7,29 @@ from urllib import request
 
 if __name__ == "__main__":
 
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    todos_req = request.Request('https://jsonplaceholder.typicode.com/todos')
-    users_req = request.Request('https://jsonplaceholder.typicode.com/users')
+    tasks = 0
+    tasks_comp = 0
+    req = request.Request('https://jsonplaceholder.typicode.com/todos')
+    req_user = request.Request('https://jsonplaceholder.typicode.com/users')
 
-    with request.urlopen(todos_req) as resp:
+    with request.urlopen(req) as resp:
         data = json.loads(resp.read().decode('utf-8'))
         for x in data:
             if x.get('userId') == int(sys.argv[1]):
-                TOTAL_NUMBER_OF_TASKS += 1
+                tasks += 1
                 if x.get('completed') is True:
-                    NUMBER_OF_DONE_TASKS += 1
+                    tasks_comp += 1
 
-    with request.urlopen(users_req) as resu:
+    with request.urlopen(req_user) as resu:
         data = json.loads(resu.read().decode('utf-8'))
         for k in data:
             if k.get('id') == int(sys.argv[1]):
-                EMPLOYEE_NAME = k['name']
+                name = k['name']
 
-    print('Employee {} is done with tasks({}/{}):'.format(EMPLOYEE_NAME,
-          NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+          tasks_comp, tasks))
 
-    with request.urlopen(todos_req) as resp:
+    with request.urlopen(req) as resp:
         data = json.loads(resp.read().decode('utf-8'))
         for x in data:
             if x.get('userId') == int(sys.argv[1]):
